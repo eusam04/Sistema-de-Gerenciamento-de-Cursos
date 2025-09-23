201e6d2746979dab2a813dfd1aeee34313b2f4fa
Samires do Carmo <samiresdocarmo@gmail.com>
2025-09-23 11:25:58 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,4 +1,4 @@
-package model;
+package entities;
 
 import java.util.Set;
 import java.util.HashSet;
@@ -24,4 +24,12 @@ public class Professor {
         inverseJoinColumns = @JoinColumn(name = "curso_id")
     )
     private Set<Curso> cursos = new HashSet<>();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
